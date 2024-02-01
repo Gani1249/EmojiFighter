d26f17e75679a6de9ab7ead3139b963687040366
Gani1249 <ganeshgc4657@gmail.com>
2024-02-01 13:13:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let fighters=["🐒","🐒","🦧","🐕","🐺","🦊","🦝","🐱","🐈‍⬛","🦁","🐯","🐆",
+            "🐴","🫎","🫏","🐎","🦄","🦓","🦓","🦬","🐂","🐃","🐄","🐖","🐏",
+            "🐑","🐐","🐪","🦙","🦒","🐘","🦣","🦏","🐁","🐇","🐿️","🦫","🦔",
+            "🐻","🐻‍❄️","🐨","🐼","🦥","🦦","🦨","🦘"];
+
+let stageEl=document.getElementById("stage");
+let fightButton=document.getElementById("fightButton");
+
+fightButton.addEventListener("click", function() {
+    let randomIndexOne=Math.floor(Math.random() * fighters.length);
+    let randomIndexTwo=Math.floor(Math.random() * fighters.length);
+    stageEl.textContent = fighters[randomIndexOne] + " VS " + fighters[randomIndexTwo];
+})
